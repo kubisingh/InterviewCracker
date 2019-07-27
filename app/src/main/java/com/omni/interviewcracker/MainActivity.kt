bea7df5f1ada8c29cf5fb85127727c5c6bd0678b
CaptHarlock <kuber.singh247@gmail.com>
2019-07-27 16:29:38 +0530

    commit last

--- app/src/main/java/com/omni/interviewcracker/MainActivity.kt
+++ app/src/main/java/com/omni/interviewcracker/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.omni.interviewcracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.omni.androidfeature.AndroidMain
+import com.omni.javafeatures.JavaMain
+import com.omni.kotlinfeatures.KotlinMain
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.setOnClickListener {
+            startActivity(Intent(baseContext, AndroidMain::class.java))
+        }
+        textView1.setOnClickListener {
+            startActivity(Intent(baseContext, JavaMain::class.java))
+        }
+        textView2.setOnClickListener {
+            startActivity(Intent(baseContext, KotlinMain::class.java))
+        }
     }
 }
